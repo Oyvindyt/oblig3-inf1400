c9de8712ac70a08327af3da33c7b783f31ad0bec
Oyvindyt <107205611+Oyvindyt@users.noreply.github.com>
2023-03-16 14:19:19 +0100

    Add files via upload

---
+++ oblig3-inf1400-v_elie/shipClass.py
@@ -0,0 +1,57 @@
+import pygame
+import math
+
+
+class Spaceship(pygame.sprite.Sprite):
+
+    BOOSTER_SPEED = 200.0
+    ROTATION_SPEED = 3.0
+    GRAVITY_SPEED = 99  # m/s^2
+
+    def __init__(self, img):
+        super().__init__()
+
+        self.image = pygame.image.load(img).convert_alpha()
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.position = pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, 0)
+        # self.gas = int(1000)
+        self.rotation = 0.0  # i radianer
+        self.rocketOn = False
+        self.shooting = False
+        self.grounded = True
+        self.bullets = []
+        
+
+    ''' Returnerer en retningsvektor for hvor skipet peker.'''
+    def forward_dir(self):
+        direction = pygame.math.Vector2(0, 1)
+        direction = direction.rotate_rad(-self.rotation)
+        return direction
+
+    ''' Metode som akselererer skipet nedover, som gravitasjon.'''
+    def gravity_vector(self):
+        self.acceleration += self.GRAVITY_SPEED * pygame.math.Vector2(0, 1)
+
+    ''' Metode for å legge til rakettens vektor.'''
+    def throttle_vector(self):
+        if self.rocketOn:
+            self.acceleration -= self.forward_dir() * self.BOOSTER_SPEED
+
+
+    def shoot(self):
+
+        if self.shooting:
+            # TODO
+            pass
+    
+    def health(self):
+        pass
+
+    def avoid_obstacle(self):
+        # TODO
+        pass
+
+
+
+    
